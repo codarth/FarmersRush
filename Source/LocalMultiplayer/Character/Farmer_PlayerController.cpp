1098f0ba426269c714b37bfb211e17e59f52f6d5
Cody <zimquo@gmail.com>
2024-02-07 15:45:31 -0700

    Added in my own ugly character with animations. Updated player character

---
+++ Source/LocalMultiplayer/Character/Farmer_PlayerController.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Farmer_PlayerController.h"
+
+AFarmer_PlayerController::AFarmer_PlayerController()
+{
+	
+}
---
+++ Source/LocalMultiplayer/Character/Farmer_PlayerController.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Farmer_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOCALMULTIPLAYER_API AFarmer_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	AFarmer_PlayerController();
+};
