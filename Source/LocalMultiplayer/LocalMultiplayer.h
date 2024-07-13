c5cdf9e47e98e5405e5398dc0b748946be7d5fc6
Cody <zimquo@gmail.com>
2024-07-13 16:43:27 -0600

    Added logging to screen macros

--- Source/LocalMultiplayer/LocalMultiplayer.h
+++ Source/LocalMultiplayer/LocalMultiplayer.h
@@ -12,4 +12,10 @@ DECLARE_LOG_CATEGORY_EXTERN(LocalMultiplayer, Log, All);
 #define LOG_WARNING(x, ...) UE_LOG(LocalMultiplayer, Warning, TEXT(x), ##__VA_ARGS__)
 #define LOG_ERROR(x, ...) UE_LOG(LocalMultiplayer, Error, TEXT(x), ##__VA_ARGS__)
 
+// Custom log with print to screen
+#define LOG_SCREEN(x, ...) UE_LOG(LocalMultiplayer, Log, TEXT(x), ##__VA_ARGS__); if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT(x), ##__VA_ARGS__));
+#define LOG_WARNING_SCREEN(x, ...) UE_LOG(LocalMultiplayer, Warning, TEXT(x), ##__VA_ARGS__); if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, FString::Printf(TEXT(x), ##__VA_ARGS__));
+#define LOG_ERROR_SCREEN(x, ...) UE_LOG(LocalMultiplayer, Error, TEXT(x), ##__VA_ARGS__); if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT(x), ##__VA_ARGS__));
+
+
 
